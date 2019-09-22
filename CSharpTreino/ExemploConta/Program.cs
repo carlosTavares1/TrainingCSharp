eda35fa6dc8d53ebf070a57fd35f2190e05c152d
Carlos Tavares <cast.3@hotmail.com>
2019-09-21 21:15:26 -0300

    Sistema academia inicializado e parado na parte de busca de dados

---
+++ CSharpTreino/ExemploConta/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExemploConta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Conta> lista = new List<Conta>();
+            Conta c1 = new Conta();
+            Conta c2 = new Conta();
+            lista.Add(c1);
+            lista.Add(c2);
+            int contador = lista.Count;
+            Console.WriteLine(contador);
+            lista.ForEach(i => Console.WriteLine(i.Numero));
+            bool temC2 = lista.Contains(c2);
+            Console.WriteLine(temC2);
+            lista.Remove(c2);
+            bool temC22 = lista.Contains(c2);
+            Console.WriteLine(temC22);
+            lista.ForEach(i => Console.WriteLine(i.Numero));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Solution1/TreinandoLista/Model/Aluno.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreinandoLista.Model
+{
+    class Aluno
+    {
+        public string Nome { get; set; }
+        public string Endereco { get; set; }
+        public int Idade { get; set; }
+        public bool Ativo { get; set; }
+        public int UsuarioCriacao { get; set; }
+        public int UsuarioAlteracao { get; set; }
+        public DateTime DataCriacao { get; set; }
+        public DateTime DataAlteracao { get; set; }
+    }
+}
