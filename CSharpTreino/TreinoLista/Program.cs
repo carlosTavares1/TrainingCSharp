8326493c0ac64ad96ccc7551064b365a18429d5a
Carlos Tavares <cast.3@hotmail.com>
2019-09-28 21:32:08 -0300

    kljlkjl

---
+++ CSharpTreino/TreinoLista/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreinoLista
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedSet<string> conjuntoOrdenado = new SortedSet<string>();
+
+            conjuntoOrdenado.Add("Marcos");
+            conjuntoOrdenado.Add("Gabriel");
+            conjuntoOrdenado.Add("Carlos");
+
+            foreach (var item in conjuntoOrdenado)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
